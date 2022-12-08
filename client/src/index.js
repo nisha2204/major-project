e94678998068e1034bb29ec45dbe7edb8a0bf44b
nisha2204 <modaninisha@gmail.com>
2022-12-08 13:09:08 +0530

    new

--- client/src/index.js
+++ client/src/index.js
@@ -1,17 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App.js";
+import Login from './login.component'  
+import RegisterBuyer from './RegisterBuyer'
+import RegisterSeller from './RegisterSeller'
+import { Router, Switch, Route, Redirect } from 'react-router-dom';
+import ThemeContextWrapper from "./components/ThemeWrapper/ThemeWrapper";
+import BackgroundColorWrapper from "./components/BackgroundColorWrapper/BackgroundColorWrapper";
+import history from './history';
+ 
+ReactDOM.render(   
+  <ThemeContextWrapper>
+  <BackgroundColorWrapper>
+    <Router history={history}>
+      <Switch>
+        <Route exact path='/' component={Login} />
+        <Route path="/RegisterBuyer" component={RegisterBuyer} />
+        <Route path="/RegisterSeller" component={RegisterSeller} />
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+      </Switch>
+    </Router>
+  </BackgroundColorWrapper>
+</ThemeContextWrapper>,
+  document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
